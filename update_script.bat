5a28f6a89442101387a1039d74438560ee62ce65
andrebids <andre@bids.pt>
2025-01-08 15:25:42 +0000

    2.3

--- update_script.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Program Files\Adobe\Adobe Illustrator 2025\Presets\en_GB\Scripts\Legenda"
-git config --global --add safe.directory "%CD%" > update_log.txt
-git fetch origin main >> update_log.txt
-git reset --hard origin/main >> update_log.txt
-git clean -fd >> update_log.txt
